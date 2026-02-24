b92e66f08dcb581c6c9f9101648a4811142d12aa
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2026-02-24 08:13:32 -0300

    Initial Commit

---
+++ iMoedas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iMoedas
+//
+//  Created by User on 24/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iMoedas/iMoedasApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMoedasApp.swift
+//  iMoedas
+//
+//  Created by User on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct iMoedasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
