d8373be99d0056e095abe38b4c9e8a14fbca78ae
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2026-02-26 17:32:45 -0300

    feat: edit view structure added

--- iMoedas/ContentView.swift
+++ iMoedas/ContentView.swift
@@ -109,6 +109,92 @@ struct CriarNovaOperacao: View {
     }
 }
 
+struct EditarOperacao: View {
+    @Binding var operacoes: [Financas]
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var titulo: String = ""
+    @State private var descricao: String = ""
+    @State private var valor: Float = 0
+    
+    struct OutlinedTextFieldStyle: TextFieldStyle {
+        func _body(configuration: TextField<Self._Label>) -> some View {
+            configuration
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .stroke(Color(UIColor.systemGray4), lineWidth: 1)
+                }
+        }
+    }
+    struct OutlinedTextFieldStyleIcon: TextFieldStyle {
+        
+        @State var icon: Image?
+        
+        func _body(configuration: TextField<Self._Label>) -> some View {
+            HStack {
+                if icon != nil {
+                    icon
+                        .foregroundColor(Color(UIColor.systemGray4))
+                }
+                configuration
+            }
+            .padding()
+            .overlay {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .stroke(Color(UIColor.systemGray4), lineWidth: 1)
+            }
+        }
+    }
+
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 24) {
+                TextField("Título", text: $titulo, axis: .vertical)
+                    .textFieldStyle(OutlinedTextFieldStyle())
+                    .previewLayout(.sizeThatFits)
+                
+                
+                TextField("Descrição", text: $descricao, axis: .vertical)
+                    .textFieldStyle(OutlinedTextFieldStyle())
+                    .previewLayout(.sizeThatFits)
+                
+                
+                TextField("Valor", value: $valor, format: .currency(code: "BRL"))
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(OutlinedTextFieldStyleIcon())
+                    .previewLayout(.sizeThatFits)
+                
+                Spacer()
+                
+            }
+            .padding()
+            .navigationTitle("Editar Transação")
+            .navigationBarTitleDisplayMode(.inline)
+            .autocorrectionDisabled(true)
+            .padding()
+            .toolbar {
+                
+                ToolbarItem(placement: .cancellationAction){
+                    Button(role: .close) {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction){
+                    
+                    Button(role: .confirm) {
+                        let newOperacao = Financas(valor: valor, titulo: titulo.trimmingCharacters(in: .whitespaces), descricao: descricao.trimmingCharacters(in: .whitespaces), categoria: "Teste", ganho: true)
+                        operacoes.append(newOperacao)
+                        dismiss()
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var operacoes : [Financas] = [
         Financas(valor: 100, titulo: "Concurso de Dança", descricao: "1º lugar na categoria de bailar", categoria: "Lazer", ganho: true),
@@ -116,6 +202,7 @@ struct ContentView: View {
     ]
     
     @State private var criarNovaOperacao = false
+    @State private var editSheet = false
     
     var body: some View {
         NavigationStack {
@@ -138,7 +225,7 @@ struct ContentView: View {
                             }
                             .swipeActions(edge: .trailing) {
                                 deleteAction(operacao.id)
-                                editAction()
+                                editAction(operacao.id)
                             }
                             
                         }
@@ -159,6 +246,9 @@ struct ContentView: View {
             .sheet(isPresented: $criarNovaOperacao) {
                 CriarNovaOperacao(operacoes: $operacoes)
             }
+            .sheet(isPresented: $editSheet) {
+                EditarOperacao(operacoes: $operacoes)
+            }
         }
         
     }
@@ -173,9 +263,9 @@ struct ContentView: View {
         }
         .tint(.red)
     }
-    private func editAction() -> some View {
+    private func editAction(_ operacaoId: Financas.ID) -> some View {
         Button {
-            // Handle edit action
+            editSheet = true
         } label: {
             VStack {
                 Image(systemName: "note.text")
