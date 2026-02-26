a1cf26580203c8bd5e18f14c0283a39fcd3045cc
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2026-02-26 10:52:51 -0300

    feat: add new transaction form created

--- iMoedas/ContentView.swift
+++ iMoedas/ContentView.swift
@@ -19,17 +19,47 @@ struct CriarNovaOperacao: View {
     @Binding var operacoes: [Financas]
     
     @Environment(\.dismiss) var dismiss
+    @State private var titulo: String = ""
+    @State private var descricao: String = ""
+    @State private var valor: Float = 0
+    
+    var formValido: Bool {
+           // Verifica se não está vazio e se não é apenas espaços
+           !titulo.trimmingCharacters(in: .whitespaces).isEmpty &&
+           !descricao.trimmingCharacters(in: .whitespaces).isEmpty
+           && valor != 0
+       }
+
     
     var body: some View {
-        Button("Adicionar teste") {
-            operacoes.append(Financas(valor: 100, titulo: "Concurso de Dança", descricao: "1º lugar na categoria de bailar", categoria: "Lazer", ganho: true))
-            dismiss()
+        
+        VStack(alignment: .leading, spacing: 30) {
+            TextField("Título", text: $titulo, axis: .vertical)
+            TextField("Descrição", text: $descricao, axis: .vertical)
+            TextField("Valor", value: $valor, format: .currency(code: "BRL"))
+                .keyboardType(.decimalPad)
+            
         }
+        .padding()
+        
+        HStack(spacing: 50){
+            Button("Cancelar") {
+                dismiss()
+            }
+            .foregroundStyle(.red)
+            Button("Salvar") {
+                let newOperacao = Financas(valor: valor, titulo: titulo, descricao: descricao, categoria: "Teste", ganho: true)
+                    operacoes.append(newOperacao)
+                    dismiss()
+            }
+            .disabled(!formValido) // Desabilita o botão se formValido for false
+        }
+
     }
 }
 
 struct ContentView: View {
-    @State private var operacoes = [
+    @State private var operacoes : [Financas] = [
         Financas(valor: 100, titulo: "Concurso de Dança", descricao: "1º lugar na categoria de bailar", categoria: "Lazer", ganho: true),
         Financas(valor: -50, titulo: "Almoço", descricao: "Mucei", categoria: "Alimentação", ganho: false)
     ]
@@ -38,23 +68,29 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            
-            List {
-                ForEach(operacoes) { operacao in
-                    
-                    VStack(alignment: .leading){
-                        LabeledContent {
-                            Text("\(operacao.valor, specifier: "%.2f")")
-                                .font(.subheadline)
-                                .foregroundColor(operacao.ganho ? Color.green : Color.red)
-                        } label: {
-                            Text("\(operacao.titulo)")
+            ZStack{
+                if operacoes.count != 0 {
+                    List {
+                        ForEach(operacoes) { operacao in
+                            
+                            VStack(alignment: .leading){
+                                LabeledContent {
+                                    Text("\(operacao.valor, specifier: "%.2f")")
+                                        .font(.subheadline)
+                                        .foregroundColor(operacao.ganho ? Color.green : Color.red)
+                                } label: {
+                                    Text("\(operacao.titulo)")
+                                    
+                                }
+                                Text("\(operacao.descricao)")
+                                    .font(.subheadline)
+                            }
                             
                         }
-                        Text("\(operacao.descricao)")
-                            .font(.subheadline)
                     }
-                    
+                }
+                else {
+                    ContentUnavailableView("Sem histórico de operações. Clique no ícone + para começar.", systemImage: "brazilianrealsign.circle.fill")
                 }
             }
             .navigationTitle(Text("iMoedas"))
