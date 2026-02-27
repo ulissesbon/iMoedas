e107675e3e52d2dfdaf73e8a325e5bb02ee92a9d
ulissesbon <ulisses.bonfim06@aluno.ifce.edu.br>
2026-02-27 09:09:05 -0300

    feat: added picker for entrada bool selection

--- iMoedas/ContentView.swift
+++ iMoedas/ContentView.swift
@@ -10,18 +10,25 @@ struct Financas: Identifiable {
     let id = UUID()
     var valor: Float
     var titulo: String
-    var descricao: String
+    var observacao: String
     var categoria: String
-    var ganho: Bool = false
+    var entrada: Bool = false
+    //    var data : Date
 }
 
 struct CriarNovaOperacao: View {
     @Binding var operacoes: [Financas]
     
     @Environment(\.dismiss) var dismiss
+    @State private var entrada: Bool = false
     @State private var titulo: String = ""
-    @State private var descricao: String = ""
+    @State private var observacao: String = ""
     @State private var valor: Float = 0
+    @State private var data: Date = Date()
+    
+    //private let categorias: [String] = ["Alimentação", "Trabalho", "Lazer", "Transporte", "Outros"]
+    
+    
     
     var formValido: Bool {
         // Verifica se não está vazio e se não é apenas espaços
@@ -60,7 +67,11 @@ struct CriarNovaOperacao: View {
         }
     }
     
+    @State var selected: String = ""
+    
+
     var body: some View {
+        let entradaOptions = [ "Entrada", "Saída"]
         
         NavigationStack{
             VStack(spacing: 24) {
@@ -69,14 +80,24 @@ struct CriarNovaOperacao: View {
                     .previewLayout(.sizeThatFits)
                 
                 
-                TextField("Descrição", text: $descricao, axis: .vertical)
-                    .textFieldStyle(OutlinedTextFieldStyle())
-                    .previewLayout(.sizeThatFits)
-                
-                
                 TextField("Valor", value: $valor, format: .currency(code: "BRL"))
                     .keyboardType(.decimalPad)
                     .textFieldStyle(OutlinedTextFieldStyleIcon())
+                    .previewLayout(.sizeThatFits)
+                
+                Picker("",
+                       selection: $selected,
+                       content: {
+                    ForEach(entradaOptions, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                ).pickerStyle(.segmented)
+
+                
+                TextField("Observações (Opcional)", text: $observacao, axis: .vertical)
+                    .textFieldStyle(OutlinedTextFieldStyle())
                     .previewLayout(.sizeThatFits)
                 
                 Spacer()
@@ -94,10 +115,13 @@ struct CriarNovaOperacao: View {
                         dismiss()
                     }
                 }
-                ToolbarItem(placement: .confirmationAction){
-                    
+                ToolbarItem(placement: .confirmationAction) {
                     Button(role: .confirm) {
-                        let newOperacao = Financas(valor: valor, titulo: titulo.trimmingCharacters(in: .whitespaces), descricao: descricao.trimmingCharacters(in: .whitespaces), categoria: "Teste", ganho: true)
+                        
+                        entrada = (selected == "Entrada") ? true : false
+                        valor = (entrada == true) ? valor : -valor
+                        
+                        let newOperacao = Financas(valor: valor, titulo: titulo.trimmingCharacters(in: .whitespaces), observacao: observacao.trimmingCharacters(in: .whitespaces), categoria: "Teste", entrada: entrada)
                         operacoes.append(newOperacao)
                         dismiss()
                     }
@@ -146,7 +170,7 @@ struct EditarOperacao: View {
             }
         }
     }
-
+    
     
     var body: some View {
         NavigationStack {
@@ -184,7 +208,7 @@ struct EditarOperacao: View {
                 ToolbarItem(placement: .confirmationAction){
                     
                     Button(role: .confirm) {
-                        let newOperacao = Financas(valor: valor, titulo: titulo.trimmingCharacters(in: .whitespaces), descricao: descricao.trimmingCharacters(in: .whitespaces), categoria: "Teste", ganho: true)
+                        let newOperacao = Financas(valor: valor, titulo: titulo.trimmingCharacters(in: .whitespaces), observacao: descricao.trimmingCharacters(in: .whitespaces), categoria: "Teste", entrada: true)
                         operacoes.append(newOperacao)
                         dismiss()
                     }
@@ -197,8 +221,8 @@ struct EditarOperacao: View {
 
 struct ContentView: View {
     @State private var operacoes : [Financas] = [
-        Financas(valor: 100, titulo: "Concurso de Dança", descricao: "1º lugar na categoria de bailar", categoria: "Lazer", ganho: true),
-        Financas(valor: -50, titulo: "Almoço", descricao: "Mucei", categoria: "Alimentação", ganho: false)
+        Financas(valor: 100, titulo: "Concurso de Dança", observacao: "1º lugar na categoria de bailar", categoria: "Lazer", entrada: true),
+        Financas(valor: -50, titulo: "Almoço", observacao: "Mucei", categoria: "Alimentação", entrada: false)
     ]
     
     @State private var criarNovaOperacao = false
@@ -215,12 +239,12 @@ struct ContentView: View {
                                 LabeledContent {
                                     Text("\(operacao.valor, specifier: "%.2f")")
                                         .font(.subheadline)
-                                        .foregroundColor(operacao.ganho ? Color.green : Color.red)
+                                        .foregroundColor(operacao.entrada ? Color.green : Color.red)
                                 } label: {
                                     Text("\(operacao.titulo)")
                                     
                                 }
-                                Text("\(operacao.descricao)")
+                                Text("\(operacao.observacao)")
                                     .font(.subheadline)
                             }
                             .swipeActions(edge: .trailing) {
@@ -257,19 +281,19 @@ struct ContentView: View {
             operacoes.removeAll(where: {$0.id == operacaoId})
         } label: {
             VStack {
-                Image(systemName: "wrongwaysign.fill")
-                Text("Delete")
+                Image(systemName: "trash.fill")
+                Text("Excluir")
             }
         }
-        .tint(.red)
+        
     }
     private func editAction(_ operacaoId: Financas.ID) -> some View {
         Button {
             editSheet = true
         } label: {
             VStack {
-                Image(systemName: "note.text")
-                Text("Edit")
+                Image(systemName: "pencil")
+                Text("Editar")
             }
         }
         .tint(Color(red: 255/255, green: 128/255, blue: 0/255))
